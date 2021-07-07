d7efd805c483bc17e6d3dd4ca6c7f44a1c50065f
kirti18patel <kirti18patel@yahoo.com>
2021-07-07 19:03:39 -0400

    checked if palindrome or not

---
+++ src/javascript/script.js
@@ -0,0 +1,28 @@
+const input = document.querySelector("#palindrome");
+const check = document.querySelector("#check");
+
+const checkIfpalindrome = userInput =>{
+    userInput = userInput.toLowerCase();
+    userInput = userInput.split(" ").join("");
+    console.log(userInput);
+    
+    let lengthOfUserInput = (userInput.length);
+    let firstPart = userInput.substring(0, Math.floor(lengthOfUserInput/2));
+    let endPart = userInput.substring(lengthOfUserInput-Math.floor(lengthOfUserInput/2));
+
+    let reverseEndPart = endPart.split("").reverse().join("");
+
+    if (firstPart===reverseEndPart){
+        document.querySelector("#result").innerHTML = "Yes! It is a palindrome.";
+    }
+    else{
+        document.querySelector("#result").innerHTML = "No! It's not a palindrome.";
+    }
+}
+
+const getUserInput = () => {
+    let userInput = input.value;
+    checkIfpalindrome(userInput);
+}
+
+check.addEventListener("click", getUserInput);
